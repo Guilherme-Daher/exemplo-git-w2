5b09619360ae2f1e6d007c91e788f61dd276b478
Guilherme-Daher <RM98611@fiap.com.br>
2023-03-07 20:53:02 -0300

    exemplo de saídas no console.

--- scirpt.js
+++ scirpt.js
@@ -1,7 +1,5 @@
 eai vitor pereira, como tá a sogra?
-
-//Amostra de saída do console com  ponteiros de marcação
-
 console.log("Olá mundo!")
 console.error("Deu erro!")
 console.warn("Atenção")
+//Amostra de saída do console com  ponteiros de marcação
